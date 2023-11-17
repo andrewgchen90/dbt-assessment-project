6165844e2f72dd1004b55c51dca6983dd573d54c
andrew chen <andrewgchen90@gmail.com>
2023-11-17 00:37:00 +0000

    materializing the right tables and views and fact tables

--- models/base/base_bq_connections.sql
+++ models/base/base_bq_connections.sql
@@ -7,7 +7,23 @@ with raw_source as (
 
 , final as (
 
-    select *
+    select 
+        id as connection_id
+        , domain
+        , dataroom_id
+        , crm_id	
+        , updated_at
+        , first_nda_signed_at
+        , created_at
+        , crm_link
+        , automated_nda_bypass
+        , bypass_justification
+        , auto_approve_requests
+        , _sdc_table_version   
+        , _sdc_batched_at             
+        , _sdc_received_at
+        , _sdc_sequence        
+        , row_number() over (partition by id order by _sdc_batched_at desc) rn
     from raw_source
 
 )
--- models/reporting/fct_questionnaire.sql
+++ models/reporting/fct_questionnaire.sql
@@ -3,10 +3,25 @@
 with joined as (
 
     select q.questionnaire_id
-        , r.*
+        , q.source_type
+        , q.due_at
+        , q.workflow_type
+        , q.original_format
+        , q.connection_id
+        , q.status
+        , r.review_id
+        , r.question_total
+        , r.issue_count
+        , r.ai_generated_accepted_answer_total
+        , r.user_generated_accepted_answer_total
+        , r.attempted_ai_generated_answer_total
+        , r.ai_generated_answer_total
+        , r.user_generated_answer_total      
     from {{ ref('int_questionnaires_most_recent') }} q
-    left join {{ ref('int_review_question_answer') }} r
-    on q.review_id = r.review_id
+    join {{ ref('int_review_question_answer') }} r
+        on q.review_id = r.review_id
+    join {{ ref('int_connections_most_recent') }} c 
+        on q.connection_id = c.connection_id
 
 )
 
